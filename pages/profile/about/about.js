1bbd050fea3460d737ac2a5069bf71f8494722aa
Devinbian <yunlong1987.hi@163.com>
2025-03-02 19:54:06 +0800

    关于我们

--- pages/profile/about/about.js
+++ pages/profile/about/about.js
@@ -1,17 +1,35 @@
-const imageUtil = require('../../../utils/image.js');
+const imageUtils = require('../../../utils/image.js');
 
 Page({
   data: {
-    imgUrls: null
+    imgUrls: null,
+    needScroll: false
   },
 
   onLoad() {
-    this.setImagesByPixelRatio();
+    this.setImagesByPixelRatio()
+  },
+
+  onReady() {
+    // this.checkNeedScroll()
   },
 
   setImagesByPixelRatio() {
-    this.setData({
-      imgUrls: imageUtil.getCommonImages('default')
-    });
+
+    this.setData({ imgUrls: imageUtils.getCommonImages('default') })
+  },
+
+  checkNeedScroll() {
+    const query = wx.createSelectorQuery()
+    query.select('.about-content').boundingClientRect()
+    query.exec(res => {
+      if (res[0]) {
+        const contentHeight = res[0].height
+        const systemInfo = wx.getSystemInfoSync()
+        const windowHeight = systemInfo.windowHeight
+        const needScroll = contentHeight > windowHeight - 180 // 180rpx是顶部padding
+        this.setData({ needScroll })
+      }
+    })
   }
-}); 
+}) 
